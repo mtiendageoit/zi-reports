676c91f2ab496d0f2eb7d88b5abd5fa2cdbd2c0c
Meliton Tienda Avila <mtienda@geoit.com.mx>
2024-08-26 22:07:44 -0600

    Version inicial de reporte basico

--- src/main/java/com/zonainmueble/reports/services/JasperReport.java
+++ src/main/java/com/zonainmueble/reports/services/JasperReport.java
@@ -1,8 +1,11 @@
 package com.zonainmueble.reports.services;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.*;
 import org.springframework.stereotype.Service;
 
 import com.zonainmueble.reports.exceptions.ReportException;
@@ -14,13 +17,41 @@ import net.sf.jasperreports.engine.*;
 @Service
 public class JasperReport {
 
+  @Value("${spring.profiles.active}")
+  private String activeProfile;
+
   public byte[] generatePdf(String sourceFileName, Map<String, Object> params) {
+    if ("dev".equals(activeProfile)) {
+      return generatePdfForDev(sourceFileName, params);
+    } else {
+      return generatePdfForProd(sourceFileName, params);
+    }
+  }
+
+  private byte[] generatePdfForProd(String sourceFileName, Map<String, Object> params) {
     try {
       params.put("REPORT_CLASS_LOADER", getClass().getClassLoader());
+
       InputStream stream = getClass().getResourceAsStream(sourceFileName);
       JasperPrint jasperPrint = JasperFillManager.fillReport(stream, params, new JREmptyDataSource());
       return JasperExportManager.exportReportToPdf(jasperPrint);
     } catch (JRException e) {
+      log.error(e.getMessage(), e);
+      log.error("sourceFileName: {}", sourceFileName);
+      log.error("params: {}", params);
+      throw new ReportException("PDF_REPORT_ERROR",
+          "An error occurred while generating the PDF report. Please try again later or contact support if the issue persists");
+    }
+  }
+
+  private byte[] generatePdfForDev(String sourceFileName, Map<String, Object> params) {
+    try {
+      Resource resource = new ClassPathResource(sourceFileName);
+      String reportPath = resource.getFile().getAbsolutePath();
+      JasperPrint jasperPrint = JasperFillManager.fillReport(reportPath, params, new JREmptyDataSource());
+
+      return JasperExportManager.exportReportToPdf(jasperPrint);
+    } catch (IOException | JRException e) {
       log.error(e.getMessage(), e);
       log.error("sourceFileName: {}", sourceFileName);
       log.error("params: {}", params);
