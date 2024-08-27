dcab1e7a069127fc38afa19cb3b65aa9b2611875
Meliton Tienda Avila <mtienda@geoit.com.mx>
2024-08-27 14:46:55 -0600

    Se agrega parametro de dirección a 2 hojas del reporte

--- src/main/java/com/zonainmueble/reports/services/BasicReportService.java
+++ src/main/java/com/zonainmueble/reports/services/BasicReportService.java
@@ -16,8 +16,16 @@ public class BasicReportService implements IReportService {
   private final JasperReport jasperReport;
 
   @Override
-  public byte[] generateReport(ReportRequestDto request) {
-    Map<String, Object> parameters = new HashMap<>();
-    return jasperReport.generatePdf(JASPER_REPORT_PATH, parameters);
+  public byte[] generateReport(ReportRequestDto input) {
+    Map<String, Object> params = new HashMap<String, Object>();
+    params.put("params", reportParams(input));
+
+    return jasperReport.generatePdf(JASPER_REPORT_PATH, params);
+  }
+
+  private Map<String, Object> reportParams(ReportRequestDto input) {
+    Map<String, Object> params = new HashMap<String, Object>();
+    params.put("address", input.getAddress());
+    return params;
   }
 }
