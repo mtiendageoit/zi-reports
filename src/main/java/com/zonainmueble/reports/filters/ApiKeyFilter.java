e860ec6e552bd8fb41edc8a88dd9364e49f8af00
Meliton Tienda Avila <mtienda@geoit.com.mx>
2024-10-08 22:20:34 -0600

    Se actualiza filtro para que solo tome en cuenta el api_key

--- src/main/java/com/zonainmueble/reports/filters/ApiKeyFilter.java
+++ src/main/java/com/zonainmueble/reports/filters/ApiKeyFilter.java
@@ -6,23 +6,12 @@ import org.springframework.stereotype.Component;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import java.io.IOException;
-import java.util.*;
 
 @Component
 public class ApiKeyFilter implements Filter {
 
   @Value("${app.api-key}")
   private String apiKey;
-
-  @Value("${app.api-allowed-urls}")
-  private String allowedUrls;
-
-  private List<String> allowedUrlList;
-
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
-    allowedUrlList = Arrays.asList(allowedUrls.split(","));
-  }
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -37,17 +26,6 @@ public class ApiKeyFilter implements Filter {
       httpResponse.getWriter().write("Invalid API Key");
       return;
     }
-
-    String referer = httpRequest.getHeader("Referer");
-
-    System.out.println("referer: " + referer);
-    // if (referer == null ||
-    // allowedUrlList.stream().noneMatch(referer::startsWith)) {
-    // httpResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
-    // httpResponse.getWriter().write("Access denied from this URL. Referer= " +
-    // referer);
-    // return;
-    // }
 
     chain.doFilter(request, response);
   }
