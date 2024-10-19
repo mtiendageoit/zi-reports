a78891476a60f40855cc17daad71ce163e2e04c5
Meliton Tienda Avila <mtienda@geoit.com.mx>
2024-10-18 20:21:46 -0600

    Se agrega icono generico de pois

--- src/main/java/com/zonainmueble/reports/models/PoisCategory.java
+++ src/main/java/com/zonainmueble/reports/models/PoisCategory.java
@@ -3,15 +3,18 @@ package com.zonainmueble.reports.models;
 import lombok.*;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class PoisCategory {
   private String key;
   private String name;
   private Integer count;
   private String icon;
 
+  public PoisCategory() {
+    this.icon = "icon-pois-generic.svg";
+  }
+
   public PoisCategory(String key, String name) {
+    this();
     this.key = key;
     this.name = name;
   }
